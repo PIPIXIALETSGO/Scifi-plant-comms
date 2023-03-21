1390867b6e4fdd80b07d0d371bccdddfa18f3dd2
John Wu <haozhu197@hotmail.com>
2023-03-21 15:53:03 -0400

    1

--- device/template-p5-project/js/script.js
+++ device/template-p5-project/js/script.js
@@ -1,33 +1,35 @@
-/**
-Title of Project
-Author Name
+var x=500
+var y=500
+function preload() {}
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
-*/
-
-"use strict";
-
-
-/**
-Description of preload
-*/
-function preload() {
-
-}
-
-
-/**
-Description of setup
-*/
 function setup() {
-
+  createCanvas(windowWidth, windowHeight);
+  background(0);
+  rect(50, 50, width - 100, 500);
 }
 
-
-/**
-Description of draw()
-*/
 function draw() {
+  background(0);
+  fill(255)
+  rect(50, 50, width - 100, 500);
+  textSize(10);
+  fill(0, 0, 255);
+
+  text(mouseX +","+ mouseY, mouseX, mouseY);
+  pipeGame();
+}
+function pipeGame() {
+    noStroke()
+  triangle(89, 97, 102, 66, 109, 97);
+  ellipse(99,95,19.5,19.5)
+  //////straight horizontal pipe//////
+fill(99, 98, 94)
+rect(x,y,5,20)
+fill(184, 181, 173)
+rect(x+5,y+1,50,18)
+fill(99, 98, 94)
+rect(x+50,y,5,20)
+  //////straight vertical pipe//////
+  
 
 }
